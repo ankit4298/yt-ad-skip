b48cd02fd2701900926c016640cf5f8c7c93fa9b
ankit4298 <ankit4298@gmail.com>
2023-06-07 18:07:20 +0530

    fixed skipAdBtn issue

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ setInterval(() => {
 	const obj = document.getElementsByClassName("ytp-ad-text");
 	if (obj.length > 0) {
 		document.getElementsByClassName('html5-main-video')[0].currentTime = document.getElementsByClassName('html5-main-video')[0].duration;
-		document.getElementsByClassName("ytp-ad-skip-button")[0].click();
+
+		const skipAdBtn = document.getElementsByClassName("ytp-ad-skip-button");
+		if(skipAdBtn.length > 0){
+			skipAdBtn[0].click();
+		}
+
 		console.log(
 			"%cAd Skipped ⏩",
 			"color:red;font-family:system-ui;font-size:20px;-webkit-text-stroke: 0.5px black;font-weight:bold"
